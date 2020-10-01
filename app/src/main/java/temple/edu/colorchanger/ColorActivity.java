7a01181e8ce1f1edd4208cbe7393a67f9e74d630
Josh <tuj84619@temple.edu>
2020-09-30 22:50:02 -0400

    The program is working as expected except for the TextView items of the spinner. The color of these is set to white when clicked, but they do not have a matching color before being selected.

--- app/src/main/java/temple/edu/colorchanger/ColorActivity.java
+++ app/src/main/java/temple/edu/colorchanger/ColorActivity.java
@@ -1,13 +1,18 @@
 package temple.edu.colorchanger;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Layout;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -16,6 +21,7 @@ public class ColorActivity extends AppCompatActivity {
     ArrayList<String> colors;
     Spinner spinner;
     TextView greetingTextView;
+    ConstraintLayout cLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,19 +29,53 @@ public class ColorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         spinner = findViewById(R.id.colorSpinner);
         colors = new ArrayList<>();
-        colors.add("Blue");
-        colors.add("Green");
-        colors.add("Red");
-        colors.add("Yellow");
+        cLayout = (ConstraintLayout)findViewById(R.id.constraintLayout);
+
+        // red, blue, green, black, white, gray, cyan, magenta, yellow, lightgray, darkgray, grey, lightgrey, darkgrey, aqua, fuchsia, lime, maroon, navy, olive, purple, silver, and teal
+        colors.add("WHITE");
+        colors.add("GREY");
+        colors.add("FUCHSIA");
+        colors.add("RED");
+        colors.add("YELLOW");
+        colors.add("LIME");
+        colors.add("GREEN");
+        colors.add("CYAN");
+        colors.add("BLUE");
+        colors.add("NAVY");
+        colors.add("MAROON");
+        colors.add("PURPLE");
+        colors.add("BLACK");
 
         final BaseAdapter adapter = new ColorAdapter(this, colors);
-        spinner.setAdapter(adapter);
 
-        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //greetingTextView.setText(parent.getItemAtPosition(position).toString());
+                Object selectedView;
+                if(parent.getItemAtPosition(position) == null){
+                    selectedView = parent.getItemAtPosition(0);
+                }
+                else{
+                    selectedView = parent.getItemAtPosition(position);
+                    view.setBackgroundColor(Color.parseColor("white"));
+                    cLayout.setBackgroundColor(Color.parseColor(selectedView.toString().toLowerCase()));
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(ColorActivity.this, "Make a selection for the background.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        /*
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 greetingTextView.setText(parent.getItemAtPosition(position).toString());
             }
-        });
+        });*/
     }
 }
